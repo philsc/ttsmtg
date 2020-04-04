a3204d64a39234003edc24b86e5534026c5fd744
Philipp Schrader <philipp.schrader@gmail.com>
2020-04-04 16:55:38 -0700

    Make basic Tabletop Simulator import work
    For some reason I'm not seeing all cards show up in the game. There
    is only a single instance of each basic land.
    
    Looking at the Scryfall API documentation they want you to do some
    caching. I'll do that with a local sqlite3 database.

--- ttsmtg.py
+++ ttsmtg.py
@@ -3,13 +3,32 @@
 import sys
 import argparse
 import re
+import json
+import sqlite3
 
 CARDNAME_REGEX = re.compile(r"(\d+ )?([^(]+) ?.*")
+CARD_BACK_URL = "TBD cardback URL"
+
+REQUEST_URL = "https://api.scryfall.com/cards/named?exact=%s"
+
+class CardDatabase():
+    def __init__(self, filename):
+        self.connection = sqlite3.connect(filename)
+        # TODO(phil): Create table and cache Scryfall queries.
+
+    def close()
+        self.connection.close()
+
+    def get(self, card_name):
+        if
 
 class Card():
     def __init__(self, quantity, name):
         self.quantity = quantity
         self.name = name
+
+    def get_url(self):
+        return "TBD cardfront URL"
 
 def parse_quantity(text):
     quantity = text.strip()
@@ -37,10 +56,63 @@ def parse_decklist(text):
 
     return cards
 
+def generate_tts_file(cards):
+    tts = dict(ObjectStates=[])
+    tts["ObjectStates"].append({
+        "ContainedObjects": [{
+            "CardId": (i + 1) * 100,
+            "Name": "Card",
+            "Nickname": card.name,
+            "Transform": {
+                "posX": 0,
+                "posY": 0,
+                "posZ": 0,
+                "rotX": 0,
+                "rotY": 180,
+                "rotZ": 180,
+                "scaleX": 1,
+                "scaleY": 1,
+                "scaleZ": 1
+            },
+        } for i, card in enumerate(cards)],
+        "CustomDeck": {
+            str(i + 1): {
+                "BackIsHidden": True,
+                "BackURL": CARD_BACK_URL,
+                "FaceURL": card.get_url(),
+                "NumHeight": 1,
+                "NumWidth": 1
+            } for i, card in enumerate(cards)
+        },
+        "DeckIDs": [
+            (i + 1) * 100 for i, card in enumerate(cards) for q in range(card.quantity)
+        ],
+        "Name": "DeckCustom",
+        "Transform": {
+            "posX": 0,
+            "posY": 1,
+            "posZ": 0,
+            "rotX": 0,
+            "rotY": 180,
+            "rotZ": 180,
+            "scaleX": 1,
+            "scaleY": 1,
+            "scaleZ": 1
+        },
+    })
+    return tts
+
 def main(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument("decklist")
     args = parser.parse_args(argv[1:])
+
+    with open(args.decklist) as file:
+        decklist = file.read()
+    cards = parse_decklist(decklist)
+    tts = generate_tts_file(cards)
+
+    print(json.dumps(tts, indent=4, sort_keys=True))
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv))
